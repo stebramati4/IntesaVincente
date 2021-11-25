45bf6de1117d48f5309764d5951e86bc63685c9b
Davide-30 <94180032+Davide-30@users.noreply.github.com>
2021-11-25 11:03:11 +0100

    Aggiunta activity inserisci parola 2

---
+++ app/src/main/java/com/example/intesavincente/InserisciParola.java
@@ -0,0 +1,14 @@
+package com.example.intesavincente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InserisciParola extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inserisci_parola);
+    }
+}

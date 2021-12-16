7dae7a3a1e9d95b0cd88ca08a27c08269279e549
mghidini <92437378+mghidini@users.noreply.github.com>
2021-12-16 10:41:53 +0100

    classi Turno e Partita

---
+++ app/src/main/java/com/example/intesavincente/MODEL/Partita.java
@@ -0,0 +1,53 @@
+package com.example.intesavincente.MODEL;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Partita {
+    private Turno[] turni;
+    private Gruppo vincitore;
+
+    public Partita() {
+        this.turni = turni;
+        this.vincitore = vincitore;
+    }
+
+    public Turno[] getTurni() {
+        return turni;
+    }
+
+    public void setTurni(Turno[] turni) {
+        this.turni = turni;
+    }
+
+    public Gruppo getVincitore() {
+        return vincitore;
+    }
+
+    public void setVincitore(Gruppo vincitore) {
+        this.vincitore = vincitore;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Partita partita = (Partita) o;
+        return Arrays.equals(turni, partita.turni) && Objects.equals(vincitore, partita.vincitore);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(vincitore);
+        result = 31 * result + Arrays.hashCode(turni);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Partita{" +
+                "turni=" + Arrays.toString(turni) +
+                ", vincitore=" + vincitore +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/example/intesavincente/MODEL/Turno.java
@@ -0,0 +1,61 @@
+package com.example.intesavincente.MODEL;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Turno {
+    private Gruppo gruppo;
+    private int passo;
+    private int parole_indovinate;
+
+    public Turno(Gruppo gruppo) {
+        this.gruppo = gruppo;
+        this.passo = 3;
+        this.parole_indovinate = 0;
+    }
+
+    public Gruppo getGruppo() {
+        return gruppo;
+    }
+
+    public void setGruppo(Gruppo gruppo) {
+        this.gruppo = gruppo;
+    }
+
+    public int getPasso() {
+        return passo;
+    }
+
+    public void setPasso(int passo) {
+        this.passo = passo;
+    }
+
+    public int getParole_indovinate() {
+        return parole_indovinate;
+    }
+
+    public void setParole_indovinate(int parole_indovinate) {
+        this.parole_indovinate = parole_indovinate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Turno turno = (Turno) o;
+        return passo == turno.passo && parole_indovinate == turno.parole_indovinate && gruppo.equals(turno.gruppo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gruppo, passo, parole_indovinate);
+    }
+
+    @Override
+    public String toString() {
+        return "Turno{" +
+                "gruppo='" + gruppo + '\'' +
+                ", passo=" + passo + '\'' +
+                ", parole indovinate=" + parole_indovinate +
+                '}';
+}

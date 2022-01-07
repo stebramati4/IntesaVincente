f5369f42d4115cf6cfd8359a2ee5ec8e52200746
rickirickiricki <94174296+rickirickiricki@users.noreply.github.com>
2022-01-07 15:40:18 +0100

    definitivo

--- app/src/main/java/com/example/intesavincente/CreaGruppoFragment.java
+++ app/src/main/java/com/example/intesavincente/CreaGruppoFragment.java
@@ -3,6 +3,7 @@ package com.example.intesavincente;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -17,8 +18,12 @@ import android.widget.Toast;
 import com.example.intesavincente.MODEL.Gruppo;
 import com.example.intesavincente.MODEL.Utente;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -98,19 +103,16 @@ public class CreaGruppoFragment extends Fragment {
     private void addGroup() {
         String nome = nomeGruppo.getText().toString();
 
-        if(!TextUtils.isEmpty(nome)){
+        if(!TextUtils.isEmpty(nome)) {
             String gruppoID = db.push().getKey();
             Gruppo gruppo = new Gruppo(nome);
             db.child("gruppi").child(gruppoID).setValue(gruppo);
             //FirebaseAuth.getInstance().getCurrentUser()
-           // String email=FirebaseAuth.getInstance().getCurrentUser().getEmail();
+            // String email=FirebaseAuth.getInstance().getCurrentUser().getEmail();
 
             //String uID=FirebaseAuth.getInstance().getCurrentUser().getUid();
 
-            //Utente u=new Utente(email,false,FirebaseAuth.getInstance().getCurrentUser().getUid());
-            //db.child("gruppi").child(gruppoID).child("utenti").setValue(u);
         }
-
         else{
            // Toast.makeText(this, "Devi inserire un nome", Toast.LENGTH_LONG).show();
         }

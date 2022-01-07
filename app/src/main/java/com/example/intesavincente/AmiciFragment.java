0ca727c6ca2ee8c332939bba43cf369a3487bc1f
stebramati4 <s.bramati2@campus.unimib.it>
2022-01-07 14:14:32 +0100

    kl

--- app/src/main/java/com/example/intesavincente/AmiciFragment.java
+++ app/src/main/java/com/example/intesavincente/AmiciFragment.java
@@ -16,6 +16,8 @@ import android.view.ViewGroup;
 public class AmiciFragment extends Fragment {
 
 
+
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";

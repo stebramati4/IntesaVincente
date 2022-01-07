0d9167116d0a5156d9e626bf6227f9f7f65a9119
Davide-30 <94180032+Davide-30@users.noreply.github.com>
2022-01-07 14:55:07 +0100

    Codice copiato da video YouTube

--- app/src/main/java/com/example/intesavincente/MainActivity.java
+++ app/src/main/java/com/example/intesavincente/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;

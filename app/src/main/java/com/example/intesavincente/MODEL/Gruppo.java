f7fc0fd561ae3ea176d09276e92543fe5735b388
Davide-30 <94180032+Davide-30@users.noreply.github.com>
2022-01-12 17:46:42 +0100

    Da questa versione l'id del gruppo e' anche un attributo del gruppo sul database. Con l'unisciti a gruppo si viene aggiunti come componente del gruppo sul databse.

--- app/src/main/java/com/example/intesavincente/MODEL/Gruppo.java
+++ app/src/main/java/com/example/intesavincente/MODEL/Gruppo.java
@@ -28,6 +28,14 @@ public class Gruppo {
         this.componenti = componenti;
     }
 
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
     public String getNome() {
         return nome;
     }
--- app/src/main/java/com/example/intesavincente/MODEL/ListaGruppiFragment.java
+++ app/src/main/java/com/example/intesavincente/MODEL/ListaGruppiFragment.java
@@ -10,11 +10,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.intesavincente.ADAPTER.ListaGruppiAdapter;
 import com.example.intesavincente.Constants;
 import com.example.intesavincente.R;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -63,8 +66,11 @@ public class ListaGruppiFragment extends Fragment {
     }
 
     ListView listaGruppi;
+    Snackbar snackbarUniscitiGruppo;
     ArrayList<Gruppo> arrayGruppi = new ArrayList<Gruppo>();
 
+    DatabaseReference dbGruppi;
+    DatabaseReference dbUtenti;
     DatabaseReference db;
 
     @Override
@@ -78,9 +84,10 @@ public class ListaGruppiFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_lista_gruppi, container, false);
+        listaGruppi = v.findViewById(R.id.Gruppi_listView);
 
-        db = FirebaseDatabase.getInstance(Constants.FIREBASE_DATABASE_URL).getReference("gruppi");
-        db.addValueEventListener(new ValueEventListener() {
+        dbGruppi = FirebaseDatabase.getInstance(Constants.FIREBASE_DATABASE_URL).getReference("gruppi");
+        dbGruppi.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) { 
                 arrayGruppi.clear();
@@ -119,13 +126,13 @@ public class ListaGruppiFragment extends Fragment {
                         Log.d(TAG, "Nome Gruppo: "+arrayGruppi.get(i).getComponenti());
                     }
 
-                    final ListaGruppiAdapter myArrayAdapter = new ListaGruppiAdapter(requireContext(), R.layout.gruppi_list_item, arrayGruppi);
 
-                    listaGruppi = v.findViewById(R.id.Gruppi_listView);
-                    listaGruppi.setAdapter(myArrayAdapter);
-                    myArrayAdapter.notifyDataSetChanged();
-                    Log.d(TAG," valore"+ gruppo.getNome()+"  "+ gruppo.getComponenti());
+
                 }
+
+                final ListaGruppiAdapter myArrayAdapter = new ListaGruppiAdapter(requireContext(), R.layout.gruppi_list_item, arrayGruppi);
+                listaGruppi.setAdapter(myArrayAdapter);
+                myArrayAdapter.notifyDataSetChanged();
 
             }
 
@@ -135,7 +142,66 @@ public class ListaGruppiFragment extends Fragment {
             }
         });
 
+        listaGruppi.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Gruppo gruppo = (Gruppo) listaGruppi.getItemAtPosition(i);
+                Log.d(TAG, "Gruppo selezionato " + listaGruppi.getItemAtPosition(i).toString());
+                Log.d(TAG, "Tipo " + listaGruppi.getItemAtPosition(i).getClass());
 
+                db = FirebaseDatabase.getInstance(Constants.FIREBASE_DATABASE_URL).getReference();
+                dbUtenti = FirebaseDatabase.getInstance(Constants.FIREBASE_DATABASE_URL).getReference("utenti");
+                dbUtenti.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        List<String> keysUtenti = new ArrayList<>();
+                        List<String> keysGruppi = new ArrayList<>();
+                        for (DataSnapshot keyNode : snapshot.getChildren()) {
+                            Log.d(TAG, "KeyNode " + keyNode);
+                            keysUtenti.add(keyNode.getKey());
+                            if (keyNode.child("idUtente").getValue().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
+                                keysUtenti.add(keyNode.getKey());
+                                Utente utente = (Utente) keyNode.getValue(Utente.class);
+                                Log.d(TAG, "Utente " + utente.toString1());
+                                Log.d(TAG, "Reference " + dbGruppi.getRef());
+                                dbGruppi.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        ArrayList<Utente> listaComponenti = new ArrayList<Utente>();
+                                        for (DataSnapshot keyNode : dataSnapshot.getChildren()) {
+                                            Log.d(TAG, "KeyNode " + keyNode);
+                                            Log.d(TAG, "Class KeyNode " + keyNode.getValue().getClass());
+                                            Log.d(TAG, "GruppoID " + keyNode.child("componenti").getValue().getClass());
+                                            listaComponenti = (ArrayList<Utente>) keyNode.child("componenti").getValue();
+                                            Log.d(TAG, "GruppoID " + listaComponenti);
+                                            keysGruppi.add(keyNode.getKey());
+                                        }
+                                        if(listaComponenti.size() < 3) {
+                                            dbGruppi.child(gruppo.getID()).child("componenti").child(String.valueOf(listaComponenti.size())).setValue(utente);
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
+
+                                    }
+                                });
+
+                            } else {
+                                // Toast.makeText(this, "Devi inserire un nome", Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+            }
+        });
 
 
         return v;

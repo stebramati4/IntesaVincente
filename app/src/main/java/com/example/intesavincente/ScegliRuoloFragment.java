f4da514609e392138e5fb8a4ed7657de4f1343d4
Davide-30 <94180032+Davide-30@users.noreply.github.com>
2022-01-27 12:11:04 +0100

    modifiche inutili ScegliRuoloFragment

--- app/src/main/java/com/example/intesavincente/ScegliRuoloFragment.java
+++ app/src/main/java/com/example/intesavincente/ScegliRuoloFragment.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class ScegliRuoloFragment extends Fragment {
 
@@ -17,17 +20,21 @@ public class ScegliRuoloFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    Button avantiButton;
+    private Button avantiButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-       View v = inflater.inflate(R.layout.fragment_scegli_ruolo, container, false);
-       avantiButton = v.findViewById(R.id.AvantiButton);
-     //  avantiButton.setOnClickListener(view -> {
-       //    Navigation.findNavController(v).navigate(R.id.);
-      // });
+        View v = inflater.inflate(R.layout.fragment_scegli_ruolo, container, false);
+        avantiButton = v.findViewById(R.id.AvantiButton);
 
-       return v;
+        avantiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return v;
     }
 }

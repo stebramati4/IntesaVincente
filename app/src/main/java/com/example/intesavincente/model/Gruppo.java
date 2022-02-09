f7b8d7972b43e6be7763f173151619a8b896c3b0
rickirickiricki <94174296+rickirickiricki@users.noreply.github.com>
2022-02-09 10:03:53 +0100

    cambio nel database: gruppi contengono array di utenti pt.2

--- app/src/main/java/com/example/intesavincente/model/Gruppo.java
+++ app/src/main/java/com/example/intesavincente/model/Gruppo.java
@@ -77,7 +77,49 @@ public class Gruppo {
             }
             else
                 return stampa;
+        } public String stampaLista(){     //Modificato in modo tale che prenda già i componenti del gruppo
+        String stampa = "";
+        for(int i=0;i<componenti.size();i++){
+            if(componenti.get(i) != null){
+                if(i == 0)
+                    stampa = componenti.get(i).toString1();
+                else
+                    stampa = stampa + ", " + componenti.get(i).toString1();
+            }
+            else
+                return stampa;
         }
+        return stampa;
+    }
+
+    public String stampaNomeComponenti(){
+        String stampa = "";
+        System.out.println("Tipo: " + componenti.getClass());
+        System.out.println("Size: " + componenti.size());
+        for(int i=0;i<componenti.size();i++){
+            Utente utente = (Utente) componenti.get(i);
+            System.out.println("Utente: " + componenti.get(i));
+            System.out.println("Tipo: " + utente.getClass());
+            System.out.println("Condizione: " + (componenti.get(i) != null));
+            if(componenti.get(i) != null){
+                if(i == 0) {
+                    System.out.println("Nickname utente if: ");
+                    stampa = componenti.get(i).getNickname();
+
+                }
+                else {
+                    System.out.println("Nickname utente else: ");
+                    stampa = stampa + ", " + componenti.get(i).getNickname();
+
+                }
+            }
+            else {
+                System.out.println("Else del primo If");
+                return stampa;
+            }
+        }
+        return stampa;
+    }
         return stampa;
     }
 
